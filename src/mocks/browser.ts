57ce20685f28e37abb9b4b053edee00c91d405c7
hong-soohyuk <username1120@naver.com>
2025-04-13 22:04:29 +0900

    feat(msw): create browser client file for proper MSW setup

---
+++ src/mocks/browser.ts
@@ -0,0 +1,8 @@
+import { setupWorker } from 'msw/browser';
+import { handlers } from './handlers';
+
+// This configures a Service Worker with the given request handlers.
+export const worker = setupWorker(...handlers);
+
+// Export for manual initialization
+export * from './handlers';
